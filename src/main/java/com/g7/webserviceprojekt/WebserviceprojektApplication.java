4620a0e4fba26c7a9277db39e501e5ce03daa82e
JimNotCarry <59293465+JimNotCarry@users.noreply.github.com>
2021-09-15 14:19:07 +0200

    Initial commit

---
+++ src/main/java/com/g7/webserviceprojekt/WebserviceprojektApplication.java
@@ -0,0 +1,13 @@
+package com.g7.webserviceprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceprojektApplication.class, args);
+	}
+
+}
