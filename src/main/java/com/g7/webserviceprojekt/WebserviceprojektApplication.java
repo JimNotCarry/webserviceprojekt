b0a11336430c871f5e24b40a868b5fe8781468fe
Johanna Salmi Sunnerdahl <73692594+Salmihanna@users.noreply.github.com>
2021-09-17 13:21:42 +0200

    Johanna test

--- src/main/java/com/g7/webserviceprojekt/WebserviceprojektApplication.java
+++ src/main/java/com/g7/webserviceprojekt/WebserviceprojektApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebserviceprojektApplication {
 
+	//johanna
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceprojektApplication.class, args);
 	}
