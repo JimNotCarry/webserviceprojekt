3ccf29fc3fb0247de772a3a2951cc862e049c250
JimNotCarry <59293465+JimNotCarry@users.noreply.github.com>
2021-09-17 13:18:39 +0200

    Commit test branch

--- src/main/java/com/g7/webserviceprojekt/WebserviceprojektApplication.java
+++ src/main/java/com/g7/webserviceprojekt/WebserviceprojektApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebserviceprojektApplication {
 
+	//Linus Heeejo
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceprojektApplication.class, args);
 	}
