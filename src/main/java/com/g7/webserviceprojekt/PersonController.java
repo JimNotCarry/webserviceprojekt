a9cc568f70e31a006a22171c7bf331d58bf85480
HA-ANgit <hannes93andersson@gmail.com>
2021-09-17 14:35:13 +0200

    Comments for the java classes

--- src/main/java/com/g7/webserviceprojekt/PersonController.java
+++ src/main/java/com/g7/webserviceprojekt/PersonController.java
@@ -2,6 +2,7 @@ package com.g7.webserviceprojekt;
 
 import org.springframework.web.bind.annotation.RestController;
 
+//Controllern är till för våra endpoints, som ropar på motoderna som ligger i vår Service
 @RestController
 public class PersonController {
 
--- src/main/java/com/g7/webserviceprojekt/PersonService.java
+++ src/main/java/com/g7/webserviceprojekt/PersonService.java
@@ -1,4 +1,6 @@
 package com.g7.webserviceprojekt;
 
+//Service är till för motoderna för vår Person. Här sker även vår eventuella connection med databasen etc
+
 public class PersonService {
 }
