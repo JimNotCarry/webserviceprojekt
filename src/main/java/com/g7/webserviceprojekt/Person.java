55b7af5fdd0d74a8f556d976d2da2355d09c8451
Johanna Salmi Sunnerdahl <73692594+Salmihanna@users.noreply.github.com>
2021-09-17 14:07:02 +0200

    Johanna, Linus, Lucas och Hannes lägger upp filerna

---
+++ src/main/java/com/g7/webserviceprojekt/Person.java
@@ -0,0 +1,50 @@
+package com.g7.webserviceprojekt;
+
+
+public class Person {
+    private int ID;
+    private String Name;
+    private String Email;
+    private String Password;
+
+    public Person() {
+    }
+    public Person(int id, String name, String email, String password) {
+        this.ID = id;
+        this.Name = name;
+        this.Email = email;
+        this.Password = password;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
