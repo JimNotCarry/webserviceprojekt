644692a5ce85a4a195a1946ff1855396ed8cd66a
JimNotCarry <59293465+JimNotCarry@users.noreply.github.com>
2021-09-20 15:31:38 +0200

    Adding and pushing Crud-repository

---
+++ src/main/java/com/g7/webserviceprojekt/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.g7.webserviceprojekt;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Accounts, Integer> {
+
+}
