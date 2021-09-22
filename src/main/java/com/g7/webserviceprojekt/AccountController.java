ff60a27155ce98353406ae348b56eaa5ecfbbaa1
Lucas Holm <lucas.e.holm@gmail.com>
2021-09-22 11:12:29 +0200

    MOB with all. Updated repository from CRUD to JPA. Added Query function so that we can search by name and email. Added search by id.

--- src/main/java/com/g7/webserviceprojekt/AccountController.java
+++ src/main/java/com/g7/webserviceprojekt/AccountController.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller // This means that this class is a Controller
-@RequestMapping(path="/accounts") // This means URL's start with /demo (after Application path)
+import java.util.List;
+
+@Controller
+@RequestMapping(path="/accounts")
 public class AccountController {
 
     @Autowired
     AccountsService accountsService;
 
-    @PostMapping(path="/add") // Map ONLY POST Requests
+    @PostMapping(path="/add")
     public @ResponseBody String addNewPerson (@RequestParam String name
             , @RequestParam String email, @RequestParam String password) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
         accountsService.addNewPerson(name,email,password);
 
         return "Hej";
@@ -27,6 +27,11 @@ public class AccountController {
         return accountsService.getAllPersons();
     }
 
+    @GetMapping(path="/id")
+    public @ResponseBody Accounts getById(int id) {
+        return accountsService.findAccountById(id);
+    }
+
     @PutMapping(path="/update")
     public @ResponseBody Accounts updateAccountByName(int id, String name, String email, String password) {
         return accountsService.updateAccount(id, name, email, password);
@@ -34,11 +39,18 @@ public class AccountController {
 
     @DeleteMapping(path="/delete")
     public @ResponseBody String deleteById(int id) {
-
         return accountsService.deleteById(id);
     }
 
+    @GetMapping(path="/name")
+    public @ResponseBody List<Accounts> findByName(String name) {
+        return accountsService.findByName(name);
+    }
 
+    @GetMapping(path="/email")
+    public @ResponseBody List<Accounts> findByEmail(String email) {
+        return accountsService.findByEmail(email);
+    }
 
 
 }
--- src/main/java/com/g7/webserviceprojekt/AccountRepository.java
+++ src/main/java/com/g7/webserviceprojekt/AccountRepository.java
@@ -1,7 +1,16 @@
 package com.g7.webserviceprojekt;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface AccountRepository extends CrudRepository<Accounts, Integer> {
+import java.util.List;
+
+public interface AccountRepository extends JpaRepository<Accounts, Integer> {
+
+    @Query ("FROM Accounts WHERE Name LIKE %?1%")
+    List<Accounts> findByNameContains(String name);
+
+    @Query ("FROM Accounts WHERE Email LIKE %?1%")
+    List<Accounts> findByEmailContains(String email);
 
 }
